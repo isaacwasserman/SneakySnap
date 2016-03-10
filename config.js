486a27528e05d0cd1810fb2bf101f24b54feddae
Isaac Wasserman <iwasserblaster@gmail.com>
2016-03-10 10:31:00 -0500

    remove

--- config.js
+++
@@ -1,9 +0,0 @@
-env = process.env.NODE_ENV;
-
-module.exports = {
-  stormpath: {
-    STORMPATH_CLIENT_APIKEY_ID: '7HUGCSDG6ICIJJ2GS9E2FP69Q',
-    STORMPATH_CLIENT_APIKEY_SECRET: 'ReKxH9BWicnaIqapSaSX3DjHh/gzgxNzFk9aKeLHUb4',
-    STORMPATH_APPLICATION_HREF: 'https://api.stormpath.com/v1/applications/YaqUfgYSVHo3s8CBRQdTW'
-  }
-}
